04d06fee8a6dba3cc0377862b5f6ca78a99afbdb
Taylor Preston <taylor.preston.music@gmail.com>
2015-11-05 15:57:15 -0600

    basic router working ... need templates

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,73 @@
 
 // *** Collections ***
-PublicCollection = Backbone.Collection.extend({
+var PublicCollection = Backbone.Collection.extend({
 });
 
-FollowingCollection = Backbone.Collection.extend({
+var FollowingCollection = Backbone.Collection.extend({
 });
 
 
 
 // *** VIEWS ***
-TimelineView = Backbone.View.extend({
+var TimelineView = Backbone.View.extend({
 });
 
-LoginView = Backbone.View.extend({
+var LoginView = Backbone.View.extend({
+  className: 'page login',
+  tagName: 'section',
+  // initialize: function(options){
+  //   console.log(options)
+  //   this.className = options.className;
+  //   this.template = options.template;
+  // },
+  render: function(){
+    this.$el.html(this.template());
+    return this;
+  }
 });
 
-RegisterView = Backbone.View.extend({
+var RegisterView = Backbone.View.extend({
 });
 
-ProfileView = Backbone.View.extend({
+var ProfileView = Backbone.View.extend({
 });
+
+// *** ROUTER ***
+
+var TwitRouter = Backbone.Router.extend({
+  routes:{
+    "":"HomeRoute",
+    "login":"LoginRoute",
+    "register":"RegisterRoute",
+    "dashboard":"DashboardRoute",
+    "profile":"ProfileRoute",
+    "users":"UsersRoute"
+  },
+  HomeRoute:  function(){
+    $('main').html('');
+  },
+  LoginRoute: function(){
+    $('main').html('');
+    $('main').html(_.template($('#loginTemplate').html()));
+  },
+  RegisterRoute  : function(){
+    $('main').html('');
+
+  },
+  DashboardRoute  : function(){
+    $('main').html('');
+
+  },
+  ProfileRoute  : function(){
+    $('main').html('');
+
+  },
+  UsersRoute  : function(){
+    $('main').html('');
+
+  },
+  });
+
+twiterRoute = new TwitRouter
+
+Backbone.history.start();
