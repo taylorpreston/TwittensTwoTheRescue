f8f9ef90e7a5da4a64efb772517d168087d2c061
Taylor Preston <taylor.preston.music@gmail.com>
2015-11-05 22:11:43 -0600

    Views and Router working, we need to look at what will be populating the view and templates :)

--- JS/main.js
+++ JS/main.js
@@ -10,26 +10,77 @@ var FollowingCollection = Backbone.Collection.extend({
 
 // *** VIEWS ***
 var TimelineView = Backbone.View.extend({
+
+  events: {
+    'click input': 'handleClick'
+  },
+
+  handleClick: function(){
+    console.log("clicked on Timeline")
+  },
+
+  render: function(){
+    console.log("rendered")
+    var template = _.template($('#timelineTemplate').html());
+    this.$el.html(template);
+    return this;
+  }
 });
 
 var LoginView = Backbone.View.extend({
   className: 'page login',
   tagName: 'section',
-  // initialize: function(options){
-  //   console.log(options)
-  //   this.className = options.className;
-  //   this.template = options.template;
-  // },
+
+  events: {
+    'click input': 'handleClick'
+  },
+
+  handleClick: function(){
+    console.log("clicked on Timeline")
+  },
+
   render: function(){
-    this.$el.html(this.template());
+    console.log("rendered")
+    var template = _.template($('#loginTemplate').html());
+    this.$el.html(template);
     return this;
   }
 });
 
 var RegisterView = Backbone.View.extend({
+
+  events: {
+    'click input': 'handleClick'
+  },
+
+  handleClick: function(){
+    console.log("clicked on Register")
+  },
+
+  render: function(){
+    console.log("rendered")
+    var template = _.template($('#registerTemplate').html());
+    this.$el.html(template);
+    return this;
+  }
 });
 
 var ProfileView = Backbone.View.extend({
+
+  events: {
+    'click input': 'handleClick'
+  },
+
+  handleClick: function(){
+    console.log("clicked on Profile")
+  },
+
+  render: function(){
+    console.log("rendered")
+    var template = _.template($('#profileTemplate').html());
+    this.$el.html(template);
+    return this;
+  }
 });
 
 // *** ROUTER ***
@@ -45,26 +96,33 @@ var TwitRouter = Backbone.Router.extend({
   },
   HomeRoute:  function(){
     $('main').html('');
+    view = new TimelineView
+    $('main').append(view.render().el);
   },
   LoginRoute: function(){
     $('main').html('');
-    $('main').html(_.template($('#loginTemplate').html()));
+    view = new LoginView
+    $('main').append(view.render().el);
   },
   RegisterRoute  : function(){
     $('main').html('');
-
+    view = new RegisterView
+    $('main').append(view.render().el);
   },
   DashboardRoute  : function(){
     $('main').html('');
-
+    view = new TimelineView
+    $('main').append(view.render().el);
   },
   ProfileRoute  : function(){
     $('main').html('');
-
+    view = new TimelineView
+    $('main').append(view.render().el);
   },
   UsersRoute  : function(){
     $('main').html('');
-
+    view = new TimelineView
+    $('main').append(view.render().el);
   },
   });
 
